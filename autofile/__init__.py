70fb5797933733455b4a0bb587db0ea109dc1373
avcopan <avcopan@gmail.com>
2019-12-24 15:26:21 -0500

    Migrates code from moldriver repo

---
+++ autofile/__init__.py
@@ -0,0 +1,13 @@
+""" autofile: filesystem schema and interface
+"""
+from autofile import file
+from autofile import info
+from autofile import system
+from autofile import fs
+
+__all__ = [
+    'file',
+    'info',
+    'system',
+    'fs',
+]
