b6ef50a48153ada0685e333201a4f5878ff5d76e
snelliott <sarah.elliott26@uga.edu>
2020-09-24 11:30:21 -0500

    attempt to make fix_ts quicker to use

--- scripts/fix_ts.py
+++ scripts/fix_ts.py
@@ -13,7 +13,7 @@ from automol.zmatrix._bimol_ts import substitution
 from automol.zmatrix._bimol_ts import addition
 from automol.zmatrix._bimol_ts import hydrogen_abstraction
 import tsfails
-
+import sys
 
 CLA = {
     'mig': hydrogen_migration,
@@ -25,14 +25,24 @@ CLA = {
     'elim': concerted_unimolecular_elimination
 }
 
+species_csv = autofile.io_.read_file(sys.argv[1]).splitlines()
+for idx, entry in enumerate(species_csv.split(',')):
+    if 'inch' in entry:
+        ich_idx = idx
 for i, fail in enumerate(tsfails.fails):
     print(fail[0])
     print(fail[1])
     print(fail[2])
-    print(fail[3])
+#    print(fail[3])
     # cnf_dirs = list(os.walk(fail[3]))[1]
-    cnf_dirs = [directory for directory in os.listdir(fail[3]) 
+    cnf_dirs = [directory for directory in os.listdir(fail[2]) 
             if directory[0] == 'c' and 'conf' not in directory]
+
+    cnf_parent = fail[2]
+    cnf_fs = autofile.fs.conformer(fail[2].replace('CONF',''))
+    inf_obj_s = cnf_fs[0].file.info.read()
+    nsampd = inf_obj_r.nsamp
+
     #cnf_dirs.remove('conf.yaml')
     print('cnf_dirs:', cnf_dirs)
     for adir in cnf_dirs:
@@ -40,8 +50,22 @@ for i, fail in enumerate(tsfails.fails):
         path = os.path.join(fail[3], adir)
         print('path:', path)
         zma_fs = autofile.fs.manager(path, 'ZMATRIX')
-        rct_geos = list(map(automol.inchi.geometry, fail[0]))
-        prd_geos = list(map(automol.inchi.geometry, fail[1]))
+        reacs = fail[0].split('=')[0].split('+')
+        prods = fail[0].split('=')[1].split('+')
+        reac_ichs = []
+        prod_ichs = []
+        for species in species_csv[1:]:
+            species = species.split()
+            for i in range(len(reacs)):
+                if species[0] == reacs[i]:
+                    reacs[i] = species[ich_idx]
+            for i in range(len(prods)):
+                if species[0] == prods[i]:
+                    prods[i] = species[ich_idx]
+        print('reacs: ', reacs)
+        print('prods: ', prods)
+        rct_geos = list(map(automol.inchi.geometry, reacs))#fail[0]))
+        prd_geos = list(map(automol.inchi.geometry, prods))#fail[1]))
         rct_zmas = list(map(automol.geom.zmatrix, rct_geos))
         prd_zmas = list(map(automol.geom.zmatrix, prd_geos))
         ret = CLA[fail[2]](rct_zmas, prd_zmas)
@@ -59,3 +83,9 @@ for i, fail in enumerate(tsfails.fails):
         print(rcts_gra)
         zma_fs[-1].file.transformation.write(tra, [0])
         zma_fs[-1].file.reactant_graph.write(rcts_gra, [0])
+        
+        tors_ranges = automol.zmatrix.torsional_sampling_ranges(tors_names)
+        tors_range_dct = dict(zip(
+            tuple(grp[0] for grp in tors_names), tors_ranges))
+        inf_obj.tors_ranges = tors_range_dct
+        cnf_fs[0].file.info.write(inf_obj)
