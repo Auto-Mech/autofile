9c4857bf158537cfbd303cfe2c522beb52ceb90d
Kevin Moore <kmoore@blueslogin4.lcrc.anl.gov>
2021-02-08 21:01:10 -0600

    update

--- scripts/rxn_chk.py
+++ scripts/rxn_chk.py
@@ -1,15 +1,26 @@
 """ Some examples of how to use the new autofile.fs functions
 """
 
+import os
 from autofile import fs
 
 
 PFX = '/lcrc/project/PACC/AutoMech/data/save/'
+# RXN_MANAGERS = fs.manager(
+#     PFX, [], 'REACTION')
 ZMA_MANAGERS = fs.iterate_managers(
     PFX,
     ['REACTION', 'THEORY', 'TRANSITION STATE', 'CONFORMER'],
     'ZMATRIX'
 )
+
+
+def new_check_rct_gra():
+    """  Check zma for trans files
+    """
+
+    for _fs in RXN_MANAGERS:
+        print(_fs)
 
 
 def check_rct_gra():
@@ -19,9 +30,10 @@ def check_rct_gra():
     no_zma_paths = []
     no_gra_paths = []
     for zma_fs in ZMA_MANAGERS:
-        zma_path = zma_fs[-1].path([0])
         if zma_fs is not None:
-            if not zma_fs[-1].file.reactant_graph.exists([0]):
+            zma_path = zma_fs[-1].path([0])
+            rct_gra_path = os.path.join(zma_path, 'zmat.g.yaml')
+            if not os.path.exists(rct_gra_path):
                 no_gra_paths.append(zma_path)
         else:
             no_zma_paths.append(zma_path)
@@ -35,4 +47,5 @@ def check_rct_gra():
 
 
 if __name__ == '__main__':
+    # new_check_rct_gra()
     check_rct_gra()
