95c0d20256a057ead70d2fb523b71c6c9b9193e6
Kevin Moore <kmoore@blueslogin4.lcrc.anl.gov>
2021-03-02 17:56:20 -0600

    adding force constant data types

--- autofile/fs.py
+++ autofile/fs.py
@@ -68,6 +68,7 @@ class _FilePrefix():
     HESS = 'hess'
     MIN = 'min'
     VPT2 = 'vpt2'
+    FC = 'fc'
     LJ = 'lj'
     IRC = 'irc'
     ZMAT = 'zmat'
@@ -103,6 +104,8 @@ class FileAttributeName():
     REAC = 'reaction'
     ANHFREQ = 'anharmonic_frequencies'
     ANHZPVE = 'anharmonic_zpve'
+    CUBIC_FC = 'cubic_force_constants'
+    QUARTIC_FC = 'quartic_force_constants'
     XMAT = 'anharmonicity_matrix'
     VIBROT_MAX = 'vibro_rot_alpha_matrix'
     CENTIF_DIST = 'quartic_centrifugal_dist_consts'
@@ -318,6 +321,8 @@ def conformer(prefix):
                 - anharmonicity_matrix
                 - vibro_rot_alpha_matrix
                 - quartic_centrifugal_dist_consts
+                - cubic_force_constants
+                - quartic_force_constants
                 - dipole_moment
                 - polarizability
 
@@ -368,6 +373,8 @@ def conformer(prefix):
     vibrot_mat_dfile = data_files.vibro_rot_alpha_matrix(_FilePrefix.VPT2)
     centrif_dist_dfile = data_files.quartic_centrifugal_dist_consts(
         _FilePrefix.VPT2)
+    # cubic_fc_dfile = data_files.cubic_force_constants(_FilePrefix.FC)
+    # quartic_fc_dfile = data_files.quartic_force_constants(_FilePrefix.FC)
     dip_mom_dfile = data_files.dipole_moment(_FilePrefix.GEOM)
     polar_dfile = data_files.polarizability(_FilePrefix.GEOM)
 
@@ -390,6 +397,8 @@ def conformer(prefix):
         FileAttributeName.XMAT: xmat_dfile,
         FileAttributeName.VIBROT_MAX: vibrot_mat_dfile,
         FileAttributeName.CENTIF_DIST: centrif_dist_dfile,
+        # FileAttributeName.CUBIC_FC: cubic_fc_dfile,
+        # FileAttributeName.QUARTIC_FC: quartic_fc_dfile,
         FileAttributeName.DIP_MOM: dip_mom_dfile,
         FileAttributeName.POLAR: polar_dfile})
 
